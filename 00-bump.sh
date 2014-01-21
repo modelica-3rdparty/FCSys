b33ba8d903bb0622a0f50ef18c61f8a9f9f6ef56
Kevin Davies <kdavies4@gmail.com>
2014-01-21 06:29:21 -1000

    Changed script to include full version number in folder name

--- 00-bump.sh
+++ 00-bump.sh
@@ -13,41 +13,34 @@
 # Kevin Davies, 7/7/13
 
 # Get the version.
-# TODO: Split the version number automatically (https://gist.github.com/pete-otaqui/4188238).
 lasttag=`git describe --abbrev=0 --tags`
-versiona=${lasttag%-*}
-versiona=${versiona%.*}
-versiona=${versiona#v*}
-versionb=${lasttag#v*.*.}
-read -p "Enter the major and minor version (last was $versiona): " versiona
-read -p "Enter the patch and optional pre-release string (last was $versionb): " versionb
-#echo v$versiona.$versionb
+read -p "Enter the new version (last was $version): " version
 
 # Get the name of the package (see assumption 1).
 package="$(basename "$( pwd )" )"
 #echo $package
 
 # Create the branch.
-git checkout -b "v$versiona.$versionb" master
+git checkout -b "v$version" master
 
 # Go to the root of the repo.
 cd "$(git rev-parse --show-toplevel)"
 
 # Update the version number in various items.
 # Package folder
-git mv "`echo $package*.*`" "$package $versiona"
+git mv "`echo $package*.*`" "$package $version"
 # Load script
-sed -i "s/$package[ 0-9.]*\/package.mo/$package $versiona\/package.mo/" load.mos
+sed -i "s/$package[ 0-9.]*\/package.mo/$package $version\/package.mo/" load.mos
 # Link to documentation
-sed -i "s/$package[ 0-9.]*\/help/$package $versiona\/help/" Documentation.html
+sed -i "s/$package[ 0-9.]*\/help/$package $version\/help/" Documentation.html
 # Readme file
-sed -i "s/($package[ 0-9.]*\//($package $versiona\//" README.md
+sed -i "s/($package[ 0-9.]*\//($package $version\//" README.md
 # Python module
-sed -i s/version='"'[0-9A-Za-z.]*'"',/version='"'$versiona.$versionb'"',/ "$package $versiona/Resources/Source/Python/setup.py"
+sed -i s/version='"'[0-9A-Za-z.]*'"',/version='"'$version'"',/ "$package $version/Resources/Source/Python/setup.py"
 # Modelica release information
 ./00-stamp.sh
 
 # Finish.
-git commit -am "Added release information for v$versiona.$versionb"
-echo "Now on branch v$versiona.$versionb from master."
+git commit -am "Added release information for v$version"
+echo "Now on branch v$version from master."
 
